0a83d99ad7a91be6ca1ad6c8f6d505767b1eb469
Oscar Baele <baeleoscar@gmail.com>
2022-03-05 02:48:21 +0000

    Original setup for backend

---
+++ app.js
@@ -0,0 +1,35 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+const port = 3000;
+const membres = require(`${__dirname}/data/membres.json`);
+const posts = require(`${__dirname}/data/blogPosts.json`);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public/"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");    
+});
+
+app.get("/info", (req, res) => {
+    res.render("information/info", {
+        allMembres: membres
+    });
+});
+
+app.get("/blog", (req, res) => {
+    res.render("blog/blog", {
+        allBlogPosts: posts
+    });
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact/contact");
+});
+
+app.listen(port, (req, res) => {
+    console.log(`App up and running listening on port ${port}`);
+});
