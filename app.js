3fc5d6f310a0560ddd25cf6fb811c75026559ae7
Oscar Baele <baeleoscar@gmail.com>
2022-03-23 13:06:26 +0000

    Merge branch 'main' of https://github.com/IdontWearBoots/Club-Code-LFJM-Dakar

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const bodyParser = require("body-parser");
+const fs = require('fs');
+const request = require('request');
 const express = require("express");
 const app = express();
 
@@ -9,6 +11,12 @@ const posts = require(`${__dirname}/data/blogPosts.json`);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public/"));
 app.set("view engine", "ejs");
+
+function downloadImage(url, filename, callback){
+  request.head(url, function(err, res, body){
+    request(url).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -28,7 +36,33 @@ app.get("/blog", (req, res) => {
 
 app.get("/blog-admin", (req, res) => {
     res.render("blogAdmin/admin.ejs");
-})
+});
+
+app.post("/blog-admin", (req, res) => {
+    console.log(req);
+    
+    let post = {};
+    
+    post["title"] = `<h1> ${req.body.postTitle} <h1>`;
+    
+    let body = req.body.postBody.split(/(\r\n)|(\n)/g).filter(e => !/(\r\n)|(\n)/g.test(e)).filter(e => e);
+    
+    console.log(body);
+    
+    post["body"] = body;
+
+    let imgName = imageSelect.slice(imageSelect.indexOf("."), imageSelect.length), 
+    
+    downloadImage(
+        res.body.imageSelect,
+        imgName, 
+        function(){ 
+            console.log(`${res.body.imageSelect} has been saved as ${imgName}`) 
+        }
+    );
+
+    res.redirect("/blog");
+});
 
 app.get("/contact", (req, res) => {
     res.render("contact/contact");
